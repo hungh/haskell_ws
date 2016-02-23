eb246f7aa43d674972ade7ee2087c13150d006e6
h.huynh <h.huynh@tmcbonds.com>
2016-02-23 16:21:08 -0500

    Initial check-in

---
+++ src/Math.hs
@@ -0,0 +1,4 @@
+module Math where
+
+absolute :: Int -> Int
+absolute n = if n < 0 then -n else n
---
+++ test/ArrayTest.hs
@@ -0,0 +1,12 @@
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Prelude" $ do
+    describe "read" $ do
+      it "can parse integers" $ do
+        read "10" `shouldBe` (10 :: Int)
+
+    describe "head" $ do
+      it "returns the first element of a list" $ do
+        head [23 ..] `shouldBe` 23
