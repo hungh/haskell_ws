29eff75f2dd0f0e0bc85e5863e1f391685faa9f8
Hung Huynh <hungcs@gmail.com>
2017-11-24 10:44:56 -0500

    show directions - turn for 2D points

--- src/exercises/streamlining/directionType.hs
+++ src/exercises/streamlining/directionType.hs
@@ -1,14 +1,24 @@
 {-- 
-
-9.
-
-Consider three two-dimensional points a, b, and c. 
+9.Consider three two-dimensional points a, b, and c. 
 If we look at the angle formed by the line segment from a to b and the line segment from b to c,
 it either turns left, turns right, or forms a straight line. 
 Define a Direction data type that lets you represent these possibilities.
 --}
 
-data Turn = Null | ToLeft | ToRight | Straight 
-data Direction a = Points [ (a,a, Turn) ]
+data Turn = ToLeft | ToRight | Straight deriving (Show)
+data Direction = Direction [Turn] deriving (Show)
+data Point a = Point { xpos:: a, ypos:: a} deriving (Show)
 
-sample = Points [ (1,2, ToRight), (6,3, ToLeft), (9,3, Null)]
+-- 10. Write a function that calculates the turn made by three 2D points and returns a Direction.
+
+calculateTurns::Ord a => Point a -> Point a -> Point a -> Direction
+calculateTurns a b c = 
+ let xa = xpos a
+     xb = xpos b
+     xc = xpos c
+ in Direction [ getTurn xa xb, getTurn xb xc]
+ 
+getTurn:: Ord a => a -> a -> Turn 
+getTurn m n = if m == n then Straight else if m > n then ToLeft else ToRight
+
+
