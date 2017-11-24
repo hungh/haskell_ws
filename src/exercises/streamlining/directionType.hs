2b1e1965c5e29e3df82832c5d3261141ba3ced3b
Hung Huynh <hungcs@gmail.com>
2017-11-24 16:59:39 -0500

    Show directions by inputing an array of 2D points

--- src/exercises/streamlining/directionType.hs
+++ src/exercises/streamlining/directionType.hs
@@ -22,3 +22,23 @@ getTurn:: Ord a => a -> a -> Turn
 getTurn m n = if m == n then Straight else if m > n then ToLeft else ToRight
 
 
+{-- 
+11. Define a function that takes a list of 2D points and computes the direction of each successive triple. 
+Given a list of points [a,b,c,d,e], it should begin by computing the turn made by [a,b,c],
+ then the turn made by [b,c,d], then [c,d,e]. Your function should return a list of Direction.
+--}
+getDirectionTripple:: Ord a => Int-> [Point a] -> Maybe Direction
+getDirectionTripple x ps 
+ | let l = length ps in (l  < 3 || x < 0 || x > l || l <= (x + 2) ) = Nothing
+ | otherwise = Just $ calculateTurns (ps!!x) (ps!!(x+1)) (ps!!(x+2))
+
+
+getDirectionTripples:: Ord a => [Point a] -> [Direction]
+getDirectionTripples [] =[]
+getDirectionTripples (x:[]) = []
+getDirectionTripples (x:y:[]) = []
+getDirectionTripples (x:y:z:rs) = [calculateTurns x y z] ++ getDirectionTripples (y:z:rs)
+
+
+
+
